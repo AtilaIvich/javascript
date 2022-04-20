076fc4e3201e10cc13a82c570e6c66ddb34c5ea1
AtilaIvich <102477378+AtilaIvich@users.noreply.github.com>
2022-04-20 13:55:59 -0300

    Exercício 08 concluído
    Exercício realizado para aprender a usar o modo de depuração. Com o modo de depuração, foi possível fazer uma comparação de funcionalidade entre os métodos de repetição.

---
+++ exercícios/ex008/ambiente.js
@@ -0,0 +1,21 @@
+for (var c=1; c <=5; c++) {
+    console.log(c);
+}
+
+console.log('Fim do _for_ e início do _while_ com teste lógico no início');
+
+var c = 1;
+while (c <= 5) {
+    console.log(c);
+    c++;
+}
+
+console.log('Fim do _while_ com teste lógico no início e início do _do while_ (teste lógico no final)');
+
+var c = 1;
+do {
+    console.log(c);
+    c++
+} while (c <= 5);
+
+console.log('Fim do _do while_ (teste lógico no final)');
